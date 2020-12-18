f0dd9ba131d3b3c29838052039829ac43fef69a4
tejaswini22199 <tejaswini22199@gmail.com>
2020-12-19 00:24:43 +0530

    randomdish

--- dish.js
+++ dish.js
@@ -2,12 +2,14 @@ var dishname;
 var inputEl=document.querySelector('.dishFind');
 var singlemeal=document.querySelector('.singleMeal');
 var searchbtn=document.querySelector('.search-btn');
-var heading=document.querySelector('.heading');
 var submit=document.getElementById('submit');
-var describe=document.querySelector('.meal-description');
+var describe=document.querySelector('.meal-list');
+var random=document.querySelector('#random');
 function searchHandler(e)
 {
+    
    e.preventDefault();
+   singlemeal.innerHTML='';
    dishname=inputEl.value;
    if(dishname.trim())
    {
@@ -16,7 +18,6 @@ function searchHandler(e)
        .then(response=>response.json())
        .then(json=>
         {
-            heading.innerHTML=`<h1>Dishes for ${dishname}</h1>`;
             console.log(json);
             if (json.meals === null) {
                 resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
@@ -25,7 +26,7 @@ function searchHandler(e)
                     json.meals.forEach(element => {
                         output+=`
                         <div>
-                        <img src="${element.strMealThumb}">
+                        <img class="image-dish" src="${element.strMealThumb}">
                         <div class="meal-info" data-mealID="${element.idMeal}">
                         <h3>${element.strMeal}</h3>
                         </div>
@@ -45,4 +46,52 @@ function searchHandler(e)
        alert('enter any item to start with');
    }
 }
+function randomHandler()
+{
+    describe.innerHTML = '';
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(response=>response.json())
+    .then(json=>
+        {
+            AddmealToUI(json.meals[0]);
+        })
+}
+function AddmealToUI(meal)
+{
+    let ingredients=[];
+    for(let i=1;i<=20;i++)
+    {
+        if(meal[`strIngredient${i}`])
+        {
+            ingredients.push(
+                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+              );
+            
+        }
+        else break;
+    }
+
+
+    singlemeal.innerHTML=`
+    <h1>${meal.strMeal}</h1>
+    <div class="mainImg">
+    <img src="${meal.strMealThumb}">
+    <div>
+    <h1>Ingredients</h1>
+    <ul>
+    ${ingredients.map((ing) =>
+        `<li>${ing}</li>`).join('')};
+    </ul>
+    </div>
+    </div>
+    <div>
+    <h1>It is a ${meal.strArea} food</h1>
+    <p>${meal.strInstructions}</p>
+    <button><a href="${meal.strYoutube}">Watch</a></button>
+    <div/>
+    `
+    
+        
+}
 submit.addEventListener('submit',searchHandler);
+random.addEventListener('click',randomHandler);
